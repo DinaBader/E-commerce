b59f86202f3618380fc9fadd2e968ca78e84cfd0
Dina Bader <dina19bader@gmail.com>
2023-12-07 00:09:56 +0200

    established a connection with the databsae

---
+++ backend/connect.php
@@ -0,0 +1,12 @@
+<?php
+$host="localhost";
+$db_user="root";
+$db_pass=null;
+$db_name="ecommerce";
+
+$mysqli=new mysqli($host,$db_user,$db_pass,$db_name);
+if($mysqli->connect_error){
+    die("".$muysqli->connect_error);
+}else{
+    
+}
