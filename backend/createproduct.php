06bb8c9ac5a2c9aae3152df8b04c96cdbadceb5f
Dina Bader <dina19bader@gmail.com>
2023-12-07 02:24:22 +0200

    seller can add products

---
+++ backend/createproduct.php
@@ -0,0 +1,59 @@
+<?php
+
+include('connect.php');
+require __DIR__ . '../../vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\Key;
+
+$headers = getallheaders();
+if (!isset($headers['Authorization']) || empty($headers['Authorization'])) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
+}
+
+$authorizationHeader = $headers['Authorization'];
+$token = null;
+
+$token = trim(str_replace("Bearer", '', $authorizationHeader));
+
+if (!$token) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
+}
+try {
+    $key = "nabiha";
+    $decoded = JWT::decode($token, new Key($key, 'HS256'));
+    $product_name = $_POST['name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    if ($decoded->usertype == 0) {
+        //seller
+        $query = $mysqli->prepare('INSERT INTO product (name, description, price) VALUES (?, ?, ?)');
+        $query->bind_param('ssi', $product_name, $description, $price);
+        $query->execute();
+        $affectedRows = $query->affected_rows;
+
+        $response = [];
+        $response["permissions"] = true;
+
+        if ($affectedRows > 0) {
+            $response["inserted"] = true;
+        } else {
+            $response["inserted"] = false;
+        }
+    } else {
+        $response = [];
+        $response["permissions"] = false;
+    }
+    echo json_encode($response);
+} catch (ExpiredException $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "expired"]);
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "Invalid token"]);
+}
